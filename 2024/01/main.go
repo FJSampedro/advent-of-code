3ca0b47da4e89497a0db3ecf0197fa8f3ed1e4db
Francisco Javier Sampedro <franjsampedro@gmail.com>
2024-12-01 20:08:21 +0000

    day 1: cleaning refactor

--- 2024/01/main.go
+++ 2024/01/main.go
@@ -11,46 +11,78 @@ import (
 
 func read_input_txt() (array1 []int, array2 []int) {
 
-	// Abre el archivo
+	// Open file
 	file, err := os.Open("input.txt")
 	if err != nil {
-		fmt.Println("Error al abrir el archivo:", err)
+		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer file.Close()
 
-	// Lector de líneas
+	// Read lines
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		// Divide la línea en columnas
+		// Strips in columns
 		columns := strings.Fields(line)
 		if len(columns) != 2 {
-			fmt.Println("Formato inválido en la línea:", line)
+			fmt.Println("Format not valid in line:", line)
 			continue
 		}
 
-		// Convierte las columnas a enteros
+		// Str to int conversion
 		num1, err1 := strconv.Atoi(columns[0])
 		num2, err2 := strconv.Atoi(columns[1])
 		if err1 != nil || err2 != nil {
-			fmt.Println("Error al convertir números en la línea:", line)
+			fmt.Println("Error in string conversion at line:", line)
 			continue
 		}
 
-		// Agrega a los arrays
+		// Adds to arrays
 		array1 = append(array1, num1)
 		array2 = append(array2, num2)
 	}
 
-	// Manejo de errores durante la lectura
+	// Error handling during reading
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Error al leer el archivo:", err)
+		fmt.Println("Error reading file:", err)
 		return
 	}
 
 	return
+}
+
+func get_total_distance(array1 []int, array2 []int) (total int) {
+
+	sort.Ints(array1)
+	sort.Ints(array2)
+
+	for i := range array1 {
+
+		if distance := array2[i] - array1[i]; distance > 0 {
+			total += distance
+		} else {
+			distance := array1[i] - array2[i]
+			total += distance
+		}
+	}
+	return total
+}
+
+func get_similarity_score(array1 []int, array2 []int) (similarity int) {
+
+	for _, num1 := range array1 {
+		var mult int
+		for _, num2 := range array2 {
+			if num1 == num2 {
+				mult++
+			}
+		}
+		similarity += num1 * mult
+	}
+
+	return similarity
 }
 
 func main() {
@@ -58,35 +90,15 @@ func main() {
 	fmt.Println("############# Part One ##############")
 	arr1, arr2 := read_input_txt()
 
-	sort.Ints(arr1)
-	sort.Ints(arr2)
+	sum := get_total_distance(arr1, arr2)
 
-	var sum int
-	for i, _ := range arr1 {
-
-		if distance := arr2[i] - arr1[i]; distance > 0 {
-			sum += distance
-		} else {
-			distance := arr1[i] - arr2[i]
-			sum += distance
-		}
-	}
 	fmt.Println("Total distance : ", sum)
 
 	fmt.Println("############# Part Two ##############")
 
 	arr1, arr2 = read_input_txt()
 
-	var sim int
-	for _, num1 := range arr1 {
-		var mult int
-		for _, num2 := range arr2 {
-			if num1 == num2 {
-				mult++
-			}
-		}
-		sim += num1 * mult
-	}
+	sim := get_similarity_score(arr1, arr2)
 
 	fmt.Println("Similarity Score : ", sim)
 }
