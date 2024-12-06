d3fda24892dc7ec07966a3658ea3ac91a62261e5
Francisco Javier Sampedro <franjsampedro@gmail.com>
2024-12-06 12:58:30 +0000

    Day 3: Part One

---
+++ 2024/03/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"lib"
+	"regexp"
+	"strconv"
+)
+
+func partOne() {
+	// Reading file
+	lines, err := lib.ReadTxtLines()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Regex commands
+	r, err := regexp.Compile(`mul\(\d*,\d*\)`)
+	if err != nil {
+		fmt.Println("Error compiling regex:", err)
+		return
+	}
+
+	var commands []string
+	for _, line := range lines {
+		commands = append(commands, r.FindAllString(line, -1)...)
+	}
+
+	//Iter commands
+	r2, err := regexp.Compile(`\d+`)
+	if err != nil {
+		fmt.Println("Error compiling regex:", err)
+		return
+	}
+
+	var sum int
+	for _, command := range commands {
+
+		multipliers := r2.FindAllString(command, -1)
+		m1, err := strconv.Atoi(multipliers[0])
+		if err != nil {
+			fmt.Println("Error converting first multiplier:", err)
+			return
+		}
+		m2, err := strconv.Atoi(multipliers[1])
+		if err != nil {
+			fmt.Println("Error converting second multiplier::", err)
+			return
+		}
+		sum += m1 * m2
+	}
+	fmt.Println(sum)
+}
+
+func main() {
+	fmt.Println("################## Part One ##################")
+	partOne()
+	fmt.Println("################## Part Two ##################")
+	// inputs = readInputTxt()
+	// partTwo(inputs)
+}
